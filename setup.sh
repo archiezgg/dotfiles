81f6fbb0b2633e2a2ca0d932c01ab43bc1f2e360
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-01-12 15:32:16 +0100

    feat(setup): remove chromium and change all aptitude to apt-get

--- setup.sh
+++ setup.sh
@@ -1,35 +1,27 @@
 # update & upgrade
-sudo apt-get install aptitude
-sudo aptitude update -y
-sudo aptitude upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # vim
-sudo aptitude install -y vim
+sudo apt-get install -y vim
 
 # docker
-sudo aptitude install -y docker.io
+sudo apt-get install -y docker.io
 
 # java
-sudo aptitude install -y openjdk-8-jdk
-sudo aptitude install -y openjdk-8-jre
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y openjdk-8-jre
 
 # python
-sudo aptitude install -y python
-sudo aptitude install -y python-pip
+sudo apt-get install -y python
+sudo apt-get install -y python-pip
 
 # thefuck
-sudo aptitude install -y python3-dev python3-pip python3-setuptools
+sudo apt-get install -y python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
 
-# atom
-sudo add-apt-repository ppa:webupd8team/atom
-sudo aptitude install -y atom
-
-# chromium
-sudo aptitude install -y chromium-browser
-
 # expect interpreter
-sudo aptitude install -y expect
+sudo apt-get install -y expect
 
 # spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
@@ -44,6 +36,6 @@ sudo apt-get install kdocker -y
 sudo cp -r configs/home/. $HOME
 
 # zsh and oh-my-zsh
-sudo aptitude install -y zsh
+sudo apt-get install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo chsh -s $(which zsh)
